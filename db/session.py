bf4fa074c32f09b09fa728f1e4b6dfbe075c1dfc
Joe Mogavero <jmogave1@jh.edu>
2025-04-16 18:12:20 -0500

    Removed MachineManager.py, which is not currently relevant to the work.

--- db/session.py
+++ db/session.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from secret import password
 
-DATABASE_URL = "mysql://root:"+password+"!@localhost:3306/gymdb"
+DATABASE_URL = "mysql://root:"+password+"@localhost:3306/gymdb"
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine)
